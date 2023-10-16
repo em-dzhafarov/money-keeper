815f1e782e63825efd1a9a34fbe7c148fa834a8b
Emil Dzhafarov <em.dzhafarov@gmail.com>
2023-10-16 16:52:29 +0200

    Updating target sdk to 34 api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.dzhafarov.moneykeeper"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dzhafarov.moneykeeper"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
